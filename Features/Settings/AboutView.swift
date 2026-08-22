8024df1c5cdc3fbf80080f27a439a76f1bf8ffae
Manhhao <manhhaoo.do@gmail.com>
2026-08-22 17:18:42 +0200

    chore: add attribution for swiftlame and kanji stroke order font

--- Features/Settings/AboutView.swift
+++ Features/Settings/AboutView.swift
@@ -87,6 +87,12 @@ struct AboutView: View {
                     text: bsdLicenseZstd
                 )
                 LicenseRow(
+                    name: "SwiftLAME",
+                    license: "LGPL-2.1",
+                    url: "https://github.com/hidden-spectrum/SwiftLAME",
+                    text: nil
+                )
+                LicenseRow(
                     name: "SwiftUI Introspect",
                     license: "MIT",
                     url: "https://github.com/siteline/swiftui-introspect",
@@ -148,6 +154,12 @@ struct AboutView: View {
                     license: "CC-BY-SA-4.0",
                     url: "https://github.com/tofugu/japanese-vocabulary-pronunciation-audio",
                     text: nil
+                )
+                LicenseRow(
+                    name: "Kanji Stroke Order Font",
+                    license: "BSD-3",
+                    url: "https://www.nihilist.org.uk",
+                    text: bsdLicenseKanjiStrokeOrders
                 )
             }
         }
@@ -221,6 +233,36 @@ struct AboutView: View {
         CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
         OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
         OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        """
+    }
+    
+    private var bsdLicenseKanjiStrokeOrders: String {
+        """
+        Copyright (C) 2004-2020 Ulrich Apel, the AAAA project and the Wadoku project
+        All rights reserved.
+        
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions
+        are met:
+        
+        1. Redistributions of source code must retain the above copyright
+           notice, this list of conditions and the following disclaimer.
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+        3. Neither the name of the author may be used to endorse or promote products
+           derived from this software without specific prior written permission.
+        
+        THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+        IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+        OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+        IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+        INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+        NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+        DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+        THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+        THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         """
     }
     
